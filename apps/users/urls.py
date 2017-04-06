981d02ef10ae92ed6cb4d21504e78571b70ddeb1
althe <zhezhe5201314@gmail.com>
2017-04-06 22:44:41 +0800

    用户中心密码修改  导航栏登陆和非登陆情况处理

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,13 +1,11 @@
 # _*_ encoding: utf-8 _*_
-
 """
 @athor: frank
 @time: 2017/4/5 20:24
 """
-# _*_ encoding:utf-8 _*_
 
 from django.conf.urls import url, include
-from users.views import UserCenterView, UploadImageView
+from users.views import UserCenterView, UploadImageView, UpdatePwdView
 
 urlpatterns = [
 
@@ -15,6 +13,8 @@ urlpatterns = [
     url(r'^info/$', UserCenterView.as_view(), name="info"),
     # 修改头像
     url(r'^image/upload/$', UploadImageView.as_view(), name="image_upload"),
+    # 用户中心修改密码
+    url(r'^update/pwd/$', UpdatePwdView.as_view(), name="update_pwd"),
 
 ]
 
